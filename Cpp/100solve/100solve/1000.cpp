cadec749ba01300ef249b7d3710c979b096f9560
Lcsvictory <yuko135z@gmail.com>
2025-04-08 11:57:01 +0900

    vector!

--- Cpp/100solve/100solve/1000.cpp
+++ Cpp/100solve/100solve/1000.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 using namespace std;
 
 int main() {
-	int a;
-	char b[4];
-	int total = 0;
-	int j = 0;
-	cin >> a >> b;
-	for (int i = sizeof(b) - 2; i >= 0; i--) {
-		total += (a * (b[i] - 48)) * (pow(10, j));
-		j++;
-		cout << a * (b[i]-48) << endl;
-	}
-	cout << total << endl;
-	
+	vector<int> a;
+
+	a.push_back(10);
+	a.push_back(20);
+
+	cout << a[0] << " " << a[1] << endl;
+
 
 	return 0;
 }
